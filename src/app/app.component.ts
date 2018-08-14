e0d5b5dc82da063b495836f21ebf2aa83bace8f1
Jurgen Van de Moere <jurgen.van.de.moere@gmail.com>
2018-08-14 09:47:07 -0400

    feat(AppComponent): add instructions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { EnvService } from 'src/app/env.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-environment-variables-demo';
+
+  constructor(
+    public env: EnvService
+  ) {
+  }
 }
